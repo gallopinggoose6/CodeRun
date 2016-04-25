47ba33ad885c0e2af8238bfbfb970baefa5976fc
gallopinggoose6 <gallopinggoose6@outlook.com>
2016-04-25 09:14:24 -0600

    Rewriting Picture Source Paths

--- src/front/output/DrawPanel.java
+++ src/front/output/DrawPanel.java
@@ -36,19 +36,19 @@ public class DrawPanel extends JPanel implements ActionListener{
         Graphics2D g2 = (Graphics2D)g;
         if(frame == 0){
             g2.drawImage(Toolkit.getDefaultToolkit().getImage
-                ("../pictures/CodeRun_Runner0.png"), 50, 50, this);
+                ("../pictures/charcterAnimation/CodeRun_Runner0.png"), 50, 50, this);
         } else if(frame == 1){
             g2.drawImage(Toolkit.getDefaultToolkit().getImage
-                ("../pictures/CodeRun_Runner1.png"), 50, 50, this);
+                ("../pictures/charcterAnimation/CodeRun_Runner1.png"), 50, 50, this);
         } else if(frame == 2){
             g2.drawImage(Toolkit.getDefaultToolkit().getImage
-                ("../pictures/CodeRun_Runner2.png"), 50, 50, this);
+                ("../pictures/charcterAnimation/CodeRun_Runner2.png"), 50, 50, this);
         } else if(frame == 3){
             g2.drawImage(Toolkit.getDefaultToolkit().getImage
-                ("../pictures/CodeRun_Runner3.png"), 50, 50, this);
+                ("../pictures/charcterAnimation/CodeRun_Runner3.png"), 50, 50, this);
         } else if(frame == 4){
             g2.drawImage(Toolkit.getDefaultToolkit().getImage
-                ("../pictures/CodeRun_Runner4.png"), 50, 50, this);
+                ("../pictures/charcterAnimation/CodeRun_Runner4.png"), 50, 50, this);
         }
     }
 
