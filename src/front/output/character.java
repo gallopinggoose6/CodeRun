b2da174e6cc2ffd9b475b094075efc4a3e049ed8
gallopinggoose6 <gallopinggoose6@outlook.com>
2016-04-25 13:45:39 -0600

    Update character.java

--- src/front/output/character.java
+++ src/front/output/character.java
@@ -1,1 +1,12 @@
+import java.awt.*;
 
+public class character {
+  int y = 50;
+	public ReturnY() {
+		return y;
+	}
+	public void DrawCharacter(Graphics g) {
+		Graphics2D g2 = (Graphics2D)g;
+	}
+}
+Status API Training Shop Blog About
