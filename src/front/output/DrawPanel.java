f7f13709aecf18b560eb7808cf29981365bc5f21
gallopinggoose6 <gallopinggoose6@outlook.com>
2016-09-09 06:56:23 -0600

    Update DrawPanel.java

--- src/front/output/DrawPanel.java
+++ src/front/output/DrawPanel.java
@@ -1,9 +1,12 @@
 package front.output;
 
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JPanel;
 
-public class DrawPanel extends JPanel {		
+public class DrawPanel extends JPanel implements KeyListener{		
     /**
      * 
      */
@@ -11,29 +14,50 @@ public class DrawPanel extends JPanel {
     static boolean blockCollisionFound = false;
     
     public DrawPanel(){
-        setBackground(Color.white);      
+    	this.setFocusable(true);
+    	this.requestFocus();
+    	addKeyListener(this);
+        setBackground(Color.white);
     }
     
     public void paintComponent(Graphics g){
         super.paintComponent(g);
         
-        Block b = new Block(50, 80, 230, 20);
-        b.Draw(g);
-        Block b1 = new Block(300, 100, 100, 100);
-        b1.Draw(g);
-        Block b2 = new Block(1000, 80, 500, 20);
-        b2.Draw(g);
-        
-        Spike s = new Spike(220, b.returnAboveY());
-        s.Draw(g);
-        Spike s2 = new Spike(1050, b2.returnAboveY());
-        s2.Draw(g);
+        drawBlocks(g);
         
         //These next two lines of code must go last
         character.Draw(g);
         blockCollisionFound = false;
     }
-    public void panelRepaint(){
-    	Frame.drawPanel.repaint();
+    public void drawBlocks(Graphics g){
+    	Block b = new Block(50, 80, 230, 20);
+        b.Draw(g);
+        Block b1 = new Block(300, 100, 100, 100);
+        b1.Draw(g);
+        Block b2 = new Block(1000, 80, 500, 20);
+        b2.Draw(g);
+        Block b3 = new Block(400, 253, 1000, 10);
+        b3.Draw(g);
+        Block b4 = new Block(550, 100, 100, 10);
+        b4.Draw(g);
+        Block b5 = new Block(750, 100, 200, 10);
+        b5.Draw(g);
+        Spike s = new Spike(800, 221);
+        s.Draw(g);
+        Spike s2 = new Spike(1050, 48);
+        s2.Draw(g);
     }
+
+	public void keyPressed(KeyEvent e) {
+		int k = e.getKeyCode();
+		if(k == KeyEvent.VK_SPACE){
+			if(character.isFalling == false){
+				character.spacePressed = true;
+				character.VELOCITY = -5;
+			}
+		}
+	}
+
+	public void keyReleased(KeyEvent e) {}
+	public void keyTyped(KeyEvent e) {}
 }
