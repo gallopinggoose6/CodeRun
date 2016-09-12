06b8d093637414135ab09d086928b6d42ae49056
gallopinggoose6 <gallopinggoose6@outlook.com>
2016-09-12 16:30:04 -0600

    Update LaunchGame.java

--- src/LaunchGame.java
+++ src/LaunchGame.java
@@ -1,13 +1,11 @@
-import back.Startup;
+import front.output.Frame;
+import front.output.character;
+import back.GameTimer;
 
-// This class launches the game.
-// It obviously is the public static void
-// It has one thing that it does, initializes the startup class;
-// More than likely the startup class will only have a consturctor
-// The reason we have a startup class and don't just put the code in this class is that I don't want to make everything static for this class.
 public class LaunchGame{
     public static void main (String[] args){
-
-        new Startup();
+    	 character character = new character();
+         GameTimer GameTimer = new GameTimer();
+         Frame frame = new Frame();
     }
 }
