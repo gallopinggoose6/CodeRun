efa4d5ff8b9e722fc75f9a5c4a5b35a18b034421
gallopinggoose6 <gallopinggoose6@outlook.com>
2017-10-16 16:54:42 -0600

    Update Frame.java

--- src/front/output/Frame.java
+++ src/front/output/Frame.java
@@ -1,17 +1,24 @@
 package front.output;
 
+import java.awt.BorderLayout;
 import java.awt.GridLayout;
 
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 
 public class Frame extends JFrame{
 	public static DrawPanel drawPanel = new DrawPanel();
+	public static TelemetryPanel telemetryPanel = new TelemetryPanel();
 	private static final long serialVersionUID = 1L;
 
-    public Frame (){        
-        add(drawPanel);
+    public Frame (){   
+    	JPanel mainPanel = new JPanel(new BorderLayout());
+    	add(mainPanel);
+        mainPanel.add(drawPanel, BorderLayout.CENTER);
+        mainPanel.add(telemetryPanel, BorderLayout.PAGE_END);
         drawPanel.setVisible(true);
+        telemetryPanel.setVisible(true);
         ImageIcon img = new ImageIcon("pictures/Stick Figure for coderunner.png");
         setIconImage(img.getImage());
         setLayout(new GridLayout(1,1));
